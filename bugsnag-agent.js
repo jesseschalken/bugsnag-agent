ef43b6aa3fd4d8db5f67fe91bd4fb7b5c18f1d13
Jesse Schalken <me@jesseschalken.com>
2015-07-07 17:29:07 +1000

    Log errors by dumping an object with keys

--- bugsnag-agent.js
+++ bugsnag-agent.js
@@ -12,10 +12,11 @@ var server = http.createServer(function (req, res) {
     var body = '';
     req.on('data', function (chunk) { body = body + chunk; });
     req.on('end', function () {
-        res.end('okay. ' + body.length + ' bytes received.');
+        var size = body.length;
+        res.end('okay. ' + size + ' bytes received.');
 
         count++;
-        console.log('recv (' + count + ' pending)');
+        console.log('recv', {size: size, pending: count});
 
         var req = https.request({
             host: 'notify.bugsnag.com',
@@ -27,7 +28,7 @@ var server = http.createServer(function (req, res) {
             res.on('data', function (chunk) { body = body + chunk; });
             res.on('end', function () {
                 count--;
-                console.log('send (' + count + ' pending)');
+                console.log('send', {size: size, pending: count});
 
                 if (res.statusCode < 200 || res.statusCode >= 300)
                     console.error(res.statusCode + ' ' + res.statusMessage + ' ' + JSON.stringify(body));
@@ -35,8 +36,7 @@ var server = http.createServer(function (req, res) {
         });
         req.on('error', function (e) {
             count--;
-            console.log('error (' + count + ' pending)');
-            console.error(e);
+            console.error('error', {size: size, pending: count, error: e});
         });
         req.end(body);
     });
